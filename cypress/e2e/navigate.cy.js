fcffa04b8f28e19ad4e2fe2765aace2ad0e7310f
Frances <frances.vanrooyen@transnet.net>
2023-03-29 18:58:17 +0200

    e2e testing updated

---
+++ cypress/e2e/navigate.cy.js
@@ -0,0 +1,8 @@
+describe('Pokedex', function() {
+    it('navigation to individual pokemon pages possible', function() {
+      cy.visit('http://localhost:5000')
+      cy.contains('ivysaur').click()
+      cy.contains('chlorophyll')
+      cy.contains('overgrow')
+    })
+  })
