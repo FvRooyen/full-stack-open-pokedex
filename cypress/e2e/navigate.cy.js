9aff2edbb62c5dcf333c61b272c97b59ae6727fb
Frances <frances.vanrooyen@transnet.net>
2023-03-29 19:56:39 +0200

    add eslint ignore fo cypress config file

--- cypress/e2e/navigate.cy.js
+++ cypress/e2e/navigate.cy.js
@@ -1,8 +1,8 @@
 describe('Pokedex', function() {
-    it('navigation to individual pokemon pages possible', function() {
-      cy.visit('http://localhost:5000')
-      cy.contains('ivysaur').click()
-      cy.contains('chlorophyll')
-      cy.contains('overgrow')
-    })
+  it('navigation to individual pokemon pages possible', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
   })
+})
