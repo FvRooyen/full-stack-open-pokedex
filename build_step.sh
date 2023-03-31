5ce7bc2d185676f423ead6b13b8bcfdf3a88a9e9
Frances <frances.vanrooyen@transnet.net>
2023-03-31 14:13:44 +0200

    changed build_step.sh

--- build_step.sh
+++ build_step.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-echo "Build script"
-
-npm install
+# Linting
+echo "Running linter..."
 npm run eslint
+
+# Build
+echo "Building app..."
 npm run build
+
+# Testing
+echo "Running tests..."
 npm test
-npm run test:e2e
+
+# Check test results
+if [ $? -eq 0 ]; then
+  echo "All tests passed"
+else
+  echo "Tests failed"
+  exit 1
+fi
